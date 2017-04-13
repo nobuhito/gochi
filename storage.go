5b04444413bea2ecb4be8ce20363060b8f9bdcfd
Nobuhito SATO <nobuhito.sato@gmail.com>
2017-04-13 19:55:45 +0900

    CloudStorage追加

---
+++ storage.go
@@ -0,0 +1,91 @@
+package gochi
+
+import (
+	"io/ioutil"
+
+	"golang.org/x/net/context"
+
+	"cloud.google.com/go/storage"
+	"google.golang.org/appengine/file"
+)
+
+type Storage struct {
+	Path string
+	Attr *storage.ObjectAttrs
+	Body []byte
+}
+
+func (s *Storage) Get(ctx context.Context) error {
+	bucketname, err := file.DefaultBucketName(ctx)
+	if err != nil {
+		return err
+	}
+
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+
+	obj := client.Bucket(bucketname).Object(s.Path)
+	s.Attr, err = obj.Attrs(ctx)
+	if err != nil {
+		return err
+	}
+
+	r, err := obj.NewReader(ctx)
+	if err != nil {
+		return err
+	}
+	defer r.Close()
+
+	body, err := ioutil.ReadAll(r)
+	if err != nil {
+		return err
+	}
+	s.Body = body
+
+	return nil
+}
+
+func (s *Storage) Write(ctx context.Context) error {
+	bucketname, err := file.DefaultBucketName(ctx)
+	if err != nil {
+		return err
+	}
+
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+
+	w := client.Bucket(bucketname).Object(s.Path).NewWriter(ctx)
+	_, err = w.Write(s.Body)
+	if err != nil {
+		return err
+	}
+	defer w.Close()
+
+	return nil
+}
+
+func (s *Storage) Delete(ctx context.Context) error {
+	bucketname, err := file.DefaultBucketName(ctx)
+	if err != nil {
+		return err
+	}
+
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+
+	err = client.Bucket(bucketname).Object(s.Path).Delete(ctx)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ storage_test.go
@@ -0,0 +1,29 @@
+package gochi
+
+//
+// Unimplemented
+//
+// func TestStorage(t *testing.T) {
+// 	g := New()
+//
+// 	text := "test"
+//
+// 	writeData := Storage{
+// 		Path: "test/test.txt",
+// 		Body: []byte(text),
+// 	}
+//
+// 	getData := Storage{
+// 		Path: "test.txt",
+// 	}
+//
+// 	_, ctx, spindown := SpinUp(t)
+// 	defer spindown()
+//
+// 	err := writeData.Write(ctx)
+// 	g.Ok(t, err)
+//
+// 	err = getData.Get(ctx)
+// 	g.Ok(t, err)
+// 	g.Equals(t, text, string(getData.Body))
+// }
